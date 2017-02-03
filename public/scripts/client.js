f19f483af95930f45c0e1b7eb671bb6a93c01735
Priscilla <Priscillayg23@gmail.com>
2017-02-03 07:36:44 -0600

    refactored code to have all API calls in service

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,48 +1,34 @@
 var app = angular.module('giphyApp', []);
 
-app.controller('GiphyController', function($http) {
+app.controller('GiphyController', function(GiphyService) {
 console.log('GiphyController loaded');
 
-var API = 'http://api.giphy.com/v1/gifs/'
 var ctrl = this;
 
-var giphyGetparams = {params: {api_key: 'dc6zaTOxFJmzC'}};
-
 ctrl.getRandomGif = function() {
-  console.log('Inside button click');
-  $http.get(API + 'random', giphyGetparams).then(function(response) {
-    console.log('Got a reponse from the API', response);
-    console.log(response.data.data.image_url);
-
-    ctrl.imageUrl = response.data.data.image_url;
-    ctrl.imageAlt = response.data.data.url;
-
-  }).catch(function(err) {
-    console.log('Error getting info from API', err);
+  // console.log("randomGif", gif);
+  GiphyService.getRandomGif().then(function(gifUrl) {
+    ctrl.randomGifUrl = gifUrl.image_url;
+    ctrl.imageAlt = gifUrl.url;
+    console.log(gifUrl);
   });
-} //end.getRandomGif
+};
+
 
 ctrl.searchGif = function(searchTerm) {
-  giphyGetparams.params.q = searchTerm;
-  giphyGetparams.params.limit = 100;
+  // GiphyService.params.q = searchTerm;
+  // GiphyService.params.limit = 100;
+  GiphyService.searchGif(searchTerm).then(function(gifUrl) {
+    ctrl.randomGifUrl = gifUrl.image_url;
+    ctrl.imageAlt = gifUrl.url;
 
-  function randomNumber() {
-    return Math.floor(Math.random() * (100));
-  }
-
-  var randomNumberIndex = randomNumber();
-
-  console.log('Inside button click');
-  $http.get(API + 'search', giphyGetparams).then(function(response) {
-    console.log('Got a search reponse from the API', response);
-    console.log(response.data.data[randomNumberIndex].images.original.url);
-
-    ctrl.imageUrl = response.data.data[randomNumberIndex].images.original.url;
-    ctrl.imageAlt = response.data.data[randomNumberIndex].url;
-
-  }).catch(function(err) {
-    console.log('Error getting search info from API', err);
   });
-}
 
-}); //end.controller
+//   var randomNumberIndex = randomNumber();
+// }
+// function randomNumber() {
+//   return Math.floor(Math.random() * (100));
+// }
+}; //end.controller
+
+});
---
+++ public/scripts/giphy-service.js
@@ -0,0 +1,35 @@
+app.service('GiphyService', function ($http) {
+
+  var API = 'http://api.giphy.com/v1/gifs/';
+  var giphyGetParams = {params: {api_key: 'dc6zaTOxFJmzC'}};
+
+  this.getRandomGif = function() {
+    console.log('Inside button click');
+    return $http.get(API + 'random', giphyGetParams).then(function(response) {
+      console.log('Got a reponse from the API', response);
+      console.log(response.data.data.image_url);
+
+      return response.data.data;
+
+    }).catch(function(err) {
+      console.log('Error getting info from API', err);
+    });
+  }; //end.getRandomGif
+
+  this.searchGif = function(searchTerm) {
+    giphyGetParams.params.tag = searchTerm;
+    console.log('Inside button click');
+    return $http.get(API + 'search', giphyGetParams).then(function(response) {
+      console.log('Got a search reponse from the API', response);
+      // console.log(response.data.data[randomNumberIndex].images.original.url);
+
+      return response.data.data;
+      // ctrl.imageAlt = response.data.data[randomNumberIndex].url;
+
+    }).catch(function(err) {
+      console.log('Error getting search info from API', err);
+    });
+
+  } //end.searchGif
+
+}); //end app.service
